cd1aeadfd36223d68fa154e5aa1e4df6c9fcc199
ErisaQ <erisabshq@gmail.com>
2025-06-05 17:43:38 +0200

    lesson 3

---
+++ lesson 3/challenge1.php
@@ -0,0 +1,12 @@
+<?php
+$b = 10;
+$a = 50;
+// $b = 50;
+
+    if ($a == $b) {
+        echo "jane te barabarta";
+    }
+    else {
+        echo "nuk jane te barabarta";
+    }
+?>
---
+++ lesson 3/challenge2.php
@@ -0,0 +1,13 @@
+<?php
+$b = 10;
+$a = 50;
+
+if($a == $b){
+    echo "1";
+} else if ($a > $b){
+    echo "2";
+}
+else {
+    echo "3";
+}
+?>
