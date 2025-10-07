9baa03c1764f2c0f1dfe00889ab3f0dc9d2ab83d
ErisaQ <erisabshq@gmail.com>
2025-10-07 17:46:36 +0200

    lesson 15

---
+++ lesson 15/delete.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once("config.php");
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM products HERE id=:id";
+
+$deleteUser = $conn->prepare($sql);
+
+$deleteUser ->bindParam(':id', $id);
+
+$deleteUser ->execute();
+
+header('Location:productDashboard.php')
+?>
